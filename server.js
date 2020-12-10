8927a9b5f3a59e49d97a5b3cf4605ee22f044353
James Y <35856061+jmyardley@users.noreply.github.com>
2020-12-10 11:23:23 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const mysql = require("mysql");
+const express = require("express");
+const app = express();
 
+const PORT = process.env.PORT || 8080;
 // mysql://fyfomaq3yaa1lg2x:gxn4fwjvi0ou2gc8@lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zacau1dtrxvo29g0
 let connection;
 
@@ -19,5 +22,12 @@ if (process.env.JAWSDB_URL) {
 connection.connect(function(err){
     if (err) throw err;
     console.log("CONNECTED");
-    connection.end();
+});
+
+app.get("/", function(req, res){
+    res.send("CONNECTED!");
+});
+
+app.listen(PORT, function() {
+    console.log("You've connected at http://localost:" + PORT);
 });
