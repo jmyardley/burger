f62967f02ca82a7dc2a23bc9c296189defa73de2
James Y <35856061+jmyardley@users.noreply.github.com>
2020-12-11 20:13:01 -0500

    restructure files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ("Big Mac");
+INSERT INTO burgers (name) VALUES ("Whopper");
+INSERT INTO burgers (name) VALUES ("Crunch Burger");
+INSERT INTO burgers (name) VALUES ("Zack's Special");
+INSERT INTO burgers (name) VALUES ("All the Way Burger");
