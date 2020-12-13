7ec2d627ca4e7bf71bfc948f9f69b962982281ae
James Y <35856061+jmyardley@users.noreply.github.com>
2020-12-13 16:32:06 -0500

    cleanup

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,3 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".devour").on("click", function(event) {
     var id = $(this).data("id");
@@ -7,35 +6,30 @@ $(function() {
       devoured: 1
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: devouredState
     }).then(
       function() {
         console.log("changed devoured to true", );
-        // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
       name: $("#ca").val().trim(),
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
         location.reload();
       }
     );
