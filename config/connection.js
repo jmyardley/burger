79b2fe19b793b3acbb718aae4572d6b9b9674375
James Y <35856061+jmyardley@users.noreply.github.com>
2020-12-12 21:20:17 -0500

    heroku test

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ var mysql = require("mysql");
 let connection;
 
 //Create connection (Add if for JAWS DB?)
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else{
+} else {
 
     connection = mysql.createConnection({
         host: "localhost",
@@ -15,7 +15,7 @@ if (process.env.JAWSDB_URL){
         password: "",
         database: "burgers_db"
     });
-    
+
 }
 connection.connect(function (err) {
     if (err) {
@@ -24,5 +24,30 @@ connection.connect(function (err) {
     }
     console.log("connected as id " + connection.threadId);
 });
+
+if (process.env.JAWSDB_URL) {
+    connection.query(`
+    DROP DATABASE IF EXISTS burgers_db;
+    CREATE DATABASE burgers_db;
+    USE burgers_db;
+    
+    CREATE TABLE burgers
+    (
+        id int NOT NULL AUTO_INCREMENT,
+        name varchar(255) NOT NULL,
+        devoured BOOLEAN DEFAULT false,
+        PRIMARY KEY (id)
+    );
+    
+    INSERT INTO burgers (name) VALUES ("Big Mac");
+    INSERT INTO burgers (name) VALUES ("Whopper");
+    INSERT INTO burgers (name) VALUES ("Crunch Burger");
+    INSERT INTO burgers (name) VALUES ("Zack's Special");
+    INSERT INTO burgers (name) VALUES ("All the Way Burger");    
+
+    `, function (err) {
+        if (err) throw err;
+    });
+}
 
 module.exports = connection;
